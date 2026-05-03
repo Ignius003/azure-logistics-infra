8a1cbd147baff6ca9919a620687b146d3304a981
Ignius003 <konparasis@gmail.com>
2026-05-03 19:27:38 +0300

    Add VNet with web and data subnets

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,3 +19,18 @@ az policy assignment create \
   --policy "871b6d14-10aa-478d-b590-94f262ecfa99" \
   --scope "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP" \
   --params scripts/policy-params.json
+
+# Step 4: Create VNet + Subnets
+az network vnet create \
+  --resource-group $RESOURCE_GROUP \
+  --name logistics-vnet \
+  --address-prefix 10.0.0.0/16 \
+  --subnet-name web-subnet \
+  --subnet-prefix 10.0.1.0/24 \
+  --tags Environment=Development
+
+az network vnet subnet create \
+  --resource-group $RESOURCE_GROUP \
+  --vnet-name logistics-vnet \
+  --name data-subnet \
+  --address-prefix 10.0.2.0/24
